3efb130ac84cba7f802bb9aa08ace0986d72091f
Seoyeon HEO <best3487@snu.ac.kr>
2026-02-22 03:11:53 +0900

    fix: dependency issues

--- src/pages/Auth/AuthPage.tsx
+++ src/pages/Auth/AuthPage.tsx
@@ -91,36 +91,3 @@ export const SignupPage = () => {
         </PinkContainer>
     )
 }
-
-	return (
-		<PinkContainer>
-			<Wrapper>
-				<img src={HeartIcon} alt="a heart icon" style={{ width: 64 }} />
-				<Title>회원가입</Title>
-				<Explanation>만나기까지의 기다림도 설렘이 될 수 있게</Explanation>
-				<Col style={{ gap: 16, width: "100%", marginTop: 50 }}>
-					<Col style={{ gap: 4 }}>
-						<InputLabel style={{ textAlign: "start" }}>이름</InputLabel>
-						<TextInput type="text" />
-					</Col>
-					<Col style={{ gap: 4 }}>
-						<InputLabel style={{ textAlign: "start" }}>이메일</InputLabel>
-						<TextInput type="email" />
-					</Col>
-					<Col style={{ gap: 4 }}>
-						<InputLabel style={{ textAlign: "start" }}>비밀번호</InputLabel>
-						<TextInput type="password" />
-					</Col>
-					<PinkButton>회원가입</PinkButton>
-					<TextButton
-						onClick={() => {
-							navigate("/login");
-						}}
-					>
-						계정이 있으신가요? 로그인
-					</TextButton>
-				</Col>
-			</Wrapper>
-		</PinkContainer>
-	);
-};
